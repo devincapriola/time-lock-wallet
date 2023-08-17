87fd8cd9f9b9aad79bdfd8e1fd83a8f1b4004b84
Devin Capriola <devincapriola@gmail.com>
2023-08-17 16:28:52 -0400

    added deposit and withdraw

--- TimeLockWallet.sol
+++ TimeLockWallet.sol
@@ -6,7 +6,11 @@ contract TimeLockWallet {
     uint256 public releaseTime;
     uint256 public lockedAmount;
 
+    event Deposited(address indexed sender, uint256 amount);
+    event Withdrawn(address indexed sender, uint256 amount);
+
     constructor(uint256 _releaseTime) {
+        require(_releaseTime > block.timestamp, "Release time must be in the future");
         owner = msg.sender;
         releaseTime = _releaseTime;
     }
@@ -21,13 +25,19 @@ contract TimeLockWallet {
         _;
     }
 
+    receive() external payable {
+        revert("Do not send Ether directly to this contract");
+    }
+
     function deposit() external payable onlyOwner {
         lockedAmount += msg.value;
+        emit Deposited(msg.sender, msg.value);
     }
 
     function withdraw() external onlyOwner onlyAfterRelease {
         uint256 amount = lockedAmount;
         lockedAmount = 0;
         payable(owner).transfer(amount);
+        emit Withdrawn(msg.sender, amount);
     }
 }
